2b35d155653b156cc3c94cf1b36b50198451da10
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 20:57:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
-def reverse_each_word(string)
-  array = string.split
-  array.each do |word|
-    new_word = word.reverse
-  end
-  return new_word.join
-end
-
 # def reverse_each_word(string)
 #   array = string.split
-#   array.collect do |word|
-#   word.reverse
+#   array.each do |word|
+#     new_word = word.reverse
 #   end
+#   return new_word.join
 # end
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect do |word|
+  word.reverse
+  end
+end
